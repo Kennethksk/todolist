5348d7eeced52a5a620fa18c37089057f2331ebf
DrDue <dr.due98@gmail.com>
2021-03-22 17:15:18 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const verifyAccount = require('../models/accounts/handlerUsers');
+const verifyAccount = require('../models/Accounts/handlerUsers');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
