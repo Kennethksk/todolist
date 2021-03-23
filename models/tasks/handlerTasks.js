743c85af22965c74064cad564d01ef520a28ba1a
DrDue <dr.due98@gmail.com>
2021-03-23 12:57:56 +0100

    Update handlerTasks.js

--- models/tasks/handlerTasks.js
+++ models/tasks/handlerTasks.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Tasks = require('./tasks');
+const Task = require('./tasks');
 
 exports.getTasks = async function(query, sort) {
     const dbname = "todolist";         // databasen hedder todolist
@@ -10,6 +10,30 @@ exports.getTasks = async function(query, sort) {
     db.once("open", function() {
     console.log("Connected to server by mongoose");
     });
-    let result = await Tasks.find(query, null, sort);
+    let result = await Task.find(query, null, sort);
     return result;
+};
+
+exports.createAccount = async function(req) {
+    const dbname = "todolist";         // databasen hedder todolist
+    const findDB = `mongodb://localhost:27017/${dbname}`;
+    const conparam = { useNewUrlParser: true, useUnifiedTopology: true };
+    await mongoose.connect(findDB, conparam);
+    const db = mongoose.connection;
+    db.once("open", function() {
+    console.log("Connected to server by mongoose");
+    });
+    
+    let task = new Task({
+        title: req.body.title,
+        description: req.body.description,
+        startDate: req.body.startDate,
+        deadline: req.body.deadline,
+    });
+    Task.create(task, function(error, savedDocument) {
+        if (error) 
+            console.log(error);
+        console.log(savedDocument);
+        db.close(); 
+    });
 };
