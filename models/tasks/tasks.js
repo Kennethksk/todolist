69005407bc50565a51e3e4061818a935c629e40b
DrDue <dr.due98@gmail.com>
2021-03-24 15:17:58 +0100

    req

--- models/tasks/tasks.js
+++ models/tasks/tasks.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const tasksSchema = mongoose.Schema({
     userID: String,
     title: String,
@@ -7,7 +8,8 @@ const tasksSchema = mongoose.Schema({
     startDate: String,
     deadline: String,
     status: {type: Boolean, default: false},
-    completionDate: String
+    completionDate: String,
+    email: req.session.email
 });
 
 module.exports = mongoose.model("Task", tasksSchema, 'tasks');
